a72c2298bc7bdf079ee2d700998ddc2f86dc75c6
Yucheng Yin <sniperyyc@sniperyycs-new-MacBook-Pro.local>
2017-11-29 15:40:19 -0500

    plate extraction to complete

---
+++ extract_plate.m
@@ -0,0 +1,13 @@
+function [I_plate] = extract_plate(I_BW)
+% Given a pre-processed black and white image, determine the location of
+% the license plate and extract it from the image
+
+% Basically, we calculate the horizontal and vertical histogram of the
+% image and find the largest width/height given some pre-defined threshold
+
+
+
+
+
+end
+
